4b81f7b66c094d2fc3616c58f29bda18f30ca979
arieeefrahman <ariefrahman01@mail.ugm.ac.id>
2021-06-26 04:03:31 +0700

    made new files

--- bot.js
+++ bot.js
@@ -1,6 +1,11 @@
 // First, dont forget to install discord.js into your project
 // by typing npm install discord.js in your terminal
-
+const {
+    monkeyFunFactsList,
+    camelFunFactsList,
+    kangarooFunFactsList,
+    pigFunFactsList
+} = require('./funFactArray');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -18,48 +23,6 @@ function readyDiscord() {
 }
 
 client.on('message', gotMessage);
-
-const repliesMessageList = [
-    '🐒',
-    '🐪',
-    '🦘',
-    '🐖'
-];
-
-const monkeyFunFactsList = [
-    'Not all primates are monkeys.',
-    'Many monkeys are at risk.',
-    'They use Grooming To Strengthen Relationships.',
-    'Only New World Monkeys Have Prehensile Tails.',
-    'There\'s Only One Species of Wild Monkey in Europe.',
-    'Pygmy Marmosets Are the World\'s Smallest Monkeys.',
-    'Mandrills Are the World\'s Largest Monkeys.'
-];
-
-const camelFunFactsList = [
-    'When a camel finally does find water, he can drink up to 40 gallons in one go.',
-    'Camels are very strong and can carry up to 900 pounds for 25 miles a day.',
-    'Camels can travel at up to 40 miles per hour – the same as a racehorse!',
-    'Don’t make a camel angry – they can spit as a way to distract whatever they think is a threat.',
-    'Mother camels carry their calves up to 14 months before giving birth.',
-    'Some calves are born completely white and turn brown as their adult coat comes in.'
-];
-
-const kangarooFunFactsList = [
-    'Most Kangaroos Are Left-Handed.',
-    'A Group of Kangaroos Is Called a Mob.',
-    'Some Kangaroos Can Hop 25 Feet.',
-    'They Can Use Their Tail as a Fifth Leg.',
-    'Joeys Can Go Dormant Until the Pouch Is Vacant.'
-];
-
-const pigFunFactsList = [
-    'Pigs are very clean animals.',
-    'Pigs can\'t sweat.',
-    'Pigs are smarter than your dog.',
-    'Mother pigs sing to their babies.',
-    'Pigs love belly rubs.'
-];
 
 function gotMessage(msg) {
     console.log(msg.content);
---
+++ funFactArray.js
@@ -0,0 +1,48 @@
+const repliesMessageList = [
+    '🐒',
+    '🐪',
+    '🦘',
+    '🐖'
+];
+
+const monkeyFunFactsList = [
+    'Not all primates are monkeys.',
+    'Many monkeys are at risk.',
+    'They use Grooming To Strengthen Relationships.',
+    'Only New World Monkeys Have Prehensile Tails.',
+    'There\'s Only One Species of Wild Monkey in Europe.',
+    'Pygmy Marmosets Are the World\'s Smallest Monkeys.',
+    'Mandrills Are the World\'s Largest Monkeys.'
+];
+
+const camelFunFactsList = [
+    'When a camel finally does find water, he can drink up to 40 gallons in one go.',
+    'Camels are very strong and can carry up to 900 pounds for 25 miles a day.',
+    'Camels can travel at up to 40 miles per hour – the same as a racehorse!',
+    'Don’t make a camel angry – they can spit as a way to distract whatever they think is a threat.',
+    'Mother camels carry their calves up to 14 months before giving birth.',
+    'Some calves are born completely white and turn brown as their adult coat comes in.'
+];
+
+const kangarooFunFactsList = [
+    'Most Kangaroos Are Left-Handed.',
+    'A Group of Kangaroos Is Called a Mob.',
+    'Some Kangaroos Can Hop 25 Feet.',
+    'They Can Use Their Tail as a Fifth Leg.',
+    'Joeys Can Go Dormant Until the Pouch Is Vacant.'
+];
+
+const pigFunFactsList = [
+    'Pigs are very clean animals.',
+    'Pigs can\'t sweat.',
+    'Pigs are smarter than your dog.',
+    'Mother pigs sing to their babies.',
+    'Pigs love belly rubs.'
+];
+
+module.exports = {
+    monkeyFunFactsList,
+    camelFunFactsList,
+    kangarooFunFactsList,
+    pigFunFactsList
+};
