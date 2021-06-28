9d8ba515fa115f8d4b42f3498ec61cfe70cdc1c1
arieeefrahman <ariefrahman01@mail.ugm.ac.id>
2021-06-28 20:58:28 +0700

    fixing bugs

--- bot.js
+++ bot.js
@@ -5,6 +5,18 @@
 
 require('dotenv').config();
 
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.login(process.env.BOTTOKEN); // Insert bot token right here || template : client.login('BOT TOKEN');
